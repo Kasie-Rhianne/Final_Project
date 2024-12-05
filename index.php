654ed764c0b2734ba56849c31c1b13215f1df1c3
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-05 12:46:01 -0500

    WIP

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 require_once 'db_connect.php'; 
 
 try {
-    $query = $pdo->query('SELECT concerts.concert_id, artists.name AS artist_name, venue.venue_name, concerts.date, concerts.time 
-    FROM concerts
-    JOIN artists ON concerts.artists_id = artists.artist_id
-    JOIN venue ON concert.venue_id = venue.venue_id 
-    LIMIT 5');
+    $query = $pdo->query('
+        SELECT 
+            concerts.concert_id, 
+            concerts.date,
+            concerts.time,
+            concerts.ticket_url,
+            artists.name AS artist_name, 
+            artists.genre AS artist_genre,
+            venue.venue_na me,
+            venue.city
+            venue.state
+        FROM concerts
+        JOIN artists ON concerts.artists_id = artists.artist_id
+        JOIN venue ON concert.venue_id = venue.venue_id
+        ORDER BY concerts.date ASC 
+        LIMIT 5
+    ');
     $items = $query->fetchAll();
 } catch (PDOException $e) {
     die('Error fetching data: ' . $e->getMessage());
