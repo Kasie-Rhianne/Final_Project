61b367825720214f658800d869dfdcbd423498ce
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-08 18:19:16 -0500

    WIP

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Music Map</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header class="hero-section">
