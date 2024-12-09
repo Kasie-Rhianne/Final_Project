ef87e131193a4495bdb6bd6a3aa2e664ef33ce34
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-09 16:19:27 -0500

    WIP

--- add_concert.php
+++ add_concert.php
@@ -1,6 +1,6 @@
 <?php
 start_session();
-require 'db_connect.php'
+require 'db_connect.php';
 
 if(!isset($_SESSION['user_id'])) {
     header("Location: login.php");
@@ -8,5 +8,16 @@ if(!isset($_SESSION['user_id'])) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+    $artist = $_POST['artist'];
+    $venue = $_POST['venue'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+
+    $sql = "INSERT INTO concerts (artist, venue, date, time) VALUES (:artist, :venue, :date, :time)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['artist' => $artist, 'venue' => $venue, 'date' => $date, 'time' => $time]);
+
+    echo "Concert added successfully!";
+    header("Location: dashboard.php");
+
 }
