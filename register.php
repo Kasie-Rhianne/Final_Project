47070f6f0200dda0a57134aabe4fe23be123e7c1
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-09 15:21:36 -0500

    WIP

--- register.php
+++ register.php
@@ -24,6 +24,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html lang ="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+<body>
+    <h1>Create an Account</h1>
+    <form method="POST">
+        <label for="username">Username:</label>
+        <input type="text" name="password" required><br>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
