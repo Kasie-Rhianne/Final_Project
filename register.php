5940c737dcdb8cf2035098c9178bf4015cb8a715
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-09 13:25:07 -0500

    WIP

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php
+include 'db_connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$username, $password]);
+
+    echo "Registration Successful!";
+}
+?>
