bcb9ce7fba8d55280cbb16c25cdc9022ca2e7bcf
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-09 16:26:51 -0500

    WIP

--- add_concert.php
+++ add_concert.php
@@ -21,3 +21,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("Location: dashboard.php");
 
 }
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Concert</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Add New Concert</h1>
+    <form method="POST">
+        <label for="artist">Artist:</label>
+        <input type="text" name="artist" required><br>
+
+        <label for="venue">Vanue:</label>
+        <input type="text" name="venue" required><br>
+
+        <label for="date">Date:</label>
+        <input type="date" name="date" required><br>
+
+        <label for="time">Time:</label>
+        <input type="time" name="time" required><br>
+
+        <button type="submit">Add Concert</button>
+    </form>
+</body>
+</html>
