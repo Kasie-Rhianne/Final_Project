fec89ede30d839cd03bcec4ba4c72b595e8697b4
Kasie-Rhianne <hazelwoodk19@mycu.concord.edu>
2024-12-09 16:56:00 -0500

    WIP

--- edit_concert.php
+++ edit_concert.php
@@ -24,7 +24,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else {
     $sql = "SELECT * FROM concerts WHERE id = :id";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id' => %id]);
+    $stmt->execute(['id' => $id]);
     $concert = $stmt->fetch();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Concert</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Edit Concert</h1>
+    <form method="POST">
+        <label for="artist">Artist:</label>
+        <input type="text" name="artist" value="<?php echo $concert['artist']; ?>" required><br>
+
+        <label for="venue">Vanue:</label>
+        <input type="text" name="venue" value="<?php echo $concert['venue']; ?>" required><br>
+
+        <label for="date">Date:</label>
+        <input type="date" name="date" value="<?php echo $concert['date']; ?>" required><br>
+
+        <label for="time">Time:</label>
+        <input type="time" name="time" value="<?php echo $concert['time']; ?>" required><br>
+
+        <button type="submit">Edit Concert</button>
+    </form>
+</body>
+</html>
